549887a5fad1fd1547d69298c1f46adf3c74dfe9
Edwin Marte Torres <kdgpro15@gmail.com>
2024-06-21 13:59:48 -0400

    feature: Refactoring to have the next projects

--- TrainingMauiFundamentals/MauiProgramExtensions.cs
+++ TrainingMauiFundamentals/MauiProgramExtensions.cs
@@ -10,14 +10,6 @@ public static class MauiProgramExtensions
 	{
 		builder
 			.UseMauiApp<App>()
-			.ConfigureLifecycleEvents(events =>
-			{
-#if ANDROID
-				events.AddAndroid(android => android.OnCreate((activity, bundle) => {
-					Debug.WriteLine("OnCreate called", "PSDEMO");
-				}));
-#endif
-			})
 			.ConfigureFonts(fonts =>
 			{
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
